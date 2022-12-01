78ccf09aba6ee5072ea1d01370237f6b2de919ad
GianlorenzoDercole <johndercole01@gmail.com>
2022-11-30 18:42:38 -0800

    button to show and hide ingredients see label.jse line 15

--- src/Ingredient.jsx
+++ src/Ingredient.jsx
@@ -1,5 +1,6 @@
+
 export default function Ingredient({ ingredient }) {
-    
+
     return(
 
         <div>
--- src/Label.jsx
+++ src/Label.jsx
@@ -1,13 +1,18 @@
 import Ingredient from './Ingredient'
+import {useState} from 'react'
 export default function Label({ recipeLabel }) {
+
+    const [showIngredients, setShowIngredients] = useState(false)
     const ingredients = recipeLabel.recipe.ingredients.map((ingredient, idx) => {
         return(<div><Ingredient ingredient={ingredient}/></div>)
     })
+
     return(
         <div>
             <h1>{recipeLabel.recipe.label}</h1>
-            <h3>{ingredients}</h3>
+            { showIngredients ? <h3>{ingredients}</h3> : ''}
             <img src={recipeLabel.recipe.image}></img>
+            <div><button onClick={() => setShowIngredients(!showIngredients)}>{showIngredients ? 'hide ingredients' : 'show ingredients'}</button></div>
         </div>
     )
 }
